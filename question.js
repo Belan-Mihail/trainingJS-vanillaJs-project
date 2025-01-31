beaf5044d1f36a960b6b95460161a1d344a59307
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-31 15:28:42 +0100

    create question.js and add 2 question as example

---
+++ question.js
@@ -0,0 +1,15 @@
+const question = [{
+    question: 'Double the elements of an array',
+    data: [1, 2, 3, 4],
+    expected_result: [2, 4, 6, 8],
+    solution: 'data.map(x => x * 2)'
+},
+{
+    question: "Double all numbers in an array",
+    data: [5, 10, 15],
+    expected: [10, 20, 30],
+    solution: "data.map(x => x * 2)"
+},
+
+
+]
