736ea54e8ccfad327313a2eb50b5a2475702b8bf
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-02-03 17:27:07 +0100

    add category to question object

--- question.js
+++ question.js
@@ -2,13 +2,15 @@ export const question = [{
     question: 'Double the elements of an array',
     data: [1, 2, 3, 4],
     expected_result: [2, 4, 6, 8],
-    solution: 'data.map(x => x * 2)'
+    solution: 'data.map(x => x * 2)',
+    category: 'map'
 },
 {
     question: "Double all numbers in an array",
     data: [5, 10, 15],
     expected_result: [10, 20, 30],
-    solution: "data.map(x => x * 2)"
+    solution: "data.map(x => x * 2)",
+    category: 'map'
 },
 
 
