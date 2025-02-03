e71d78a1a55d446ca57b2bcea04b244f64ad2276
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-02-03 17:57:37 +0100

    add 8 new questions

--- question.js
+++ question.js
@@ -1,4 +1,5 @@
-export const question = [{
+export const question = [
+    {
     question: 'Double the elements of an array',
     data: [1, 2, 3, 4],
     expected_result: [2, 4, 6, 8],
@@ -10,6 +11,62 @@ export const question = [{
     data: [5, 10, 15],
     expected_result: [10, 20, 30],
     solution: "data.map(x => x * 2)",
+    category: 'map'
+},
+{
+    question: 'Add 5 to each element in the array',
+    data: [1, 2, 3, 4],
+    expected_result: [6, 7, 8, 9],
+    solution: 'data.map(x => x + 5)',
+    category: 'map'
+},
+{
+    question: 'Convert all strings in the array to uppercase',
+    data: ['apple', 'banana', 'cherry'],
+    expected_result: ['APPLE', 'BANANA', 'CHERRY'],
+    solution: 'data.map(x => x.toUpperCase())',
+    category: 'map'
+},
+{
+    question: 'Square all numbers in the array',
+    data: [2, 4, 6],
+    expected_result: [4, 16, 36],
+    solution: 'data.map(x => x ** 2)',
+    category: 'map'
+},
+{
+    question: 'Extract the length of each string in the array',
+    data: ['hello', 'world', 'JavaScript'],
+    expected_result: [5, 5, 10],
+    solution: 'data.map(x => x.length)',
+    category: 'map'
+},
+{
+    question: 'Convert an array of numbers to their string representation',
+    data: [1, 2, 3, 4],
+    expected_result: ['1', '2', '3', '4'],
+    solution: 'data.map(String)',
+    category: 'map'
+},
+{
+    question: 'Get the first letter of each string in the array',
+    data: ['dog', 'cat', 'bird'],
+    expected_result: ['d', 'c', 'b'],
+    solution: 'data.map(x => x[0])',
+    category: 'map'
+},
+{
+    question: 'Convert all numbers to their negative equivalents',
+    data: [1, 2, 3, 4],
+    expected_result: [-1, -2, -3, -4],
+    solution: 'data.map(x => -x)',
+    category: 'map'
+},
+{
+    question: 'Double all even numbers in the array',
+    data: [1, 2, 3, 4, 5],
+    expected_result: [1, 4, 3, 8, 5],
+    solution: 'data.map(x => x % 2 === 0 ? x * 2 : x)',
     category: 'map'
 },
 
